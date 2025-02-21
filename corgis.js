a69b3b6e668b462358c0bac2e35781c18b1af885
BrookeBlack <brooke.black@example.com>
2025-02-20 20:53:35 -0600

    updated corgis.js

---
+++ corgis.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function corgiChanger() {
+    corgiButton.innerHTML = "You Changed me!";
+    corgiP.innerHTML = "Cardigan Welsh Corgis!"
+  }
+  
+  corgiButton.onclick = corgiChanger;
